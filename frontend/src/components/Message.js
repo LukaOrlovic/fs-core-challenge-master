7108d6420c4797dd3589d7c848dc4070ec535c00
LukaOrlovic <orlovicluka@gmail.com>
2023-12-10 22:04:58 +0100

    Resolved issue with displaying sender name

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Message = ({ message, isUsersMessage }) => {
     return (
         <div className={isUsersMessage ? 'users-message' : 'container'}>
-            <p style={{ fontWeight: "lighter"}}>{message.sender}</p>
+            <p style={{ fontWeight: "lighter"}}>{isUsersMessage ? "You" : message.sender}</p>
             <p style={{ fontWeight: "bold"}}> {message.text}</p>
             <p class="lighttext">{message.timestamp}</p>
         </div>
